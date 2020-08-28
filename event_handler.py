9674578e1fd1133b1ec38cecb538231d2cc95cd5
ReubenHawley <reubenhawley@gmail.com>
2020-08-28 21:16:02 +0200

    create entry order and exit order list append trade price to the list remove trades from entry and exit if symbol price > trade price reset order values to None if successfully captured, this prevents data leakage. fix bug where portfolio only capturing single entry. check each trade if the trade size is not greater tha nt the free balance

--- event_handler.py
+++ event_handler.py
@@ -1,39 +1,41 @@
-from exchange import Exchange
+# from exchange import Exchange
 from read_email import EmailScanner
 from time import time, sleep
 from Portfolio import Portfolio
 import sys
 import ccxt
-
+global exit_order
 
 if __name__ == '__main__':
     print('Welcome to Autobot:')
+    binance = ccxt.binance()
+    open_entry_order_list = []
+    open_exit_order_list = []
+    trade_history = Portfolio()
     while True:
         try:
             print('connecting to exchange')
             # mex = Exchange()
             print('reading executed trades')
-            trade_history = Portfolio()
             print('scanning emails')
             read_emails = EmailScanner()
             parameters = read_emails.email_scanner()
+            free_balance = binance.fetch_free_balance()
             if parameters is not None:
-                if 1 == 1:  #mex.available_balance() > 0:  # TODO see if requested trade size !> than available balance
+                if parameters[3] > free_balance:
                     # order = mex.executeTrade(parameters[0], parameters[1],parameters[2], parameters[3])
                     # simulating the order as it is received and the response
-                    binance = ccxt.binance()
-                    ticker = binance.fetch_ticker(symbol='BTC/USDT')
-
+                    ticker = binance.fetch_ticker(symbol='BTC/USDT')  # fetch the latest symbol price
                     entry_order = {
                         'timestamp': round(time()),
                         'orderID': round(time()),
                         'symbol': parameters[0],
-                        'price':ticker['close'],
+                        'price': ticker['close'],
                         'side': parameters[2],
                         'ordType': parameters[1],
                         'orderQty': parameters[3]
                     }
-                    print(entry_order)
+                    print(f'executed trade: \n{entry_order}')
                     if "error" not in entry_order:
                         read_emails.Send_report(entry_order)
                         read_emails.imap.delete_message(read_emails.CountEmails())
@@ -42,6 +44,7 @@ if __name__ == '__main__':
                                          order_dict['symbol'], order_dict['price'], order_dict['side'],
                                          order_dict['ordType'], order_dict['orderQty']]
                         trade_history.ADD_TRADE(order_details)  # adds the order to the portfolio
+                        open_entry_order_list.append(float(order_dict['price']))
                         trade_history.CREATE_CSV()
                         sell_price = order_details[3]*1.025
                         sellQTY = float(order_dict['orderQty'])*1.025
@@ -55,15 +58,25 @@ if __name__ == '__main__':
                             'ordType': 'limit',
                             'orderQty': sellQTY
                         }
-                        trade_history.ADD_TRADE(exit_order)  # adds the order to the portfolio
-                        trade_history.CREATE_CSV()
-                        print(exit_order)
-                        read_emails.Send_report(exit_order)
+                        print(f'submitted exit order:\n{exit_order}')
+                        open_exit_order_list.append(float(exit_order['price']))
                         parameters = None
+                        entry_order = None
+                        order_dict = None
+                        order_details = None
                         sleep(5)
                     else:
                         print('error encountered, retrying')
                         continue
+            if open_exit_order_list is not None:
+                for key, placed_exit_order in enumerate(open_exit_order_list):
+                    ticker = binance.fetch_ticker(symbol='BTC/USDT')
+                    if ticker['close'] > placed_exit_order:
+                        trade_history.ADD_TRADE(exit_order)  # adds the order to the portfolio
+                        read_emails.Send_report(exit_order)
+                        open_entry_order_list.__delitem__(key)
+                        open_exit_order_list.__delitem__(key)
+                        exit_order = None
             else:
                 sleep(5)
                 continue
@@ -72,6 +85,6 @@ if __name__ == '__main__':
             sleep(10)
         except:
             e = sys.exc_info()[0]
-            print("<p>Error: %s</p>" % e)
-            #read_emails.Send_report(e, f'{e.args}')
+            print("Error: %s" % e)
+            # read_emails.Send_report(e, f'{e.args}')
             continue
