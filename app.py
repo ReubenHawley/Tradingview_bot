fe5b5cddccb564109ad2a55e5903dc6bfe678e73
ReubenHawley <reubenhawley@gmail.com>
2021-01-06 00:08:38 +0100

    removed Portfolio.py rectify the bug causing order to call itself. set tp to 2.5% round the exit price to fit the binance api call

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template
 import ccxt
 import os
-import pandas as pd
 from ast import literal_eval
-from Portfolio import Portfolio
+from flask_ngrok import run_with_ngrok
 
+# Start ngrok when app is run
 try:
     c_dir = os.path.dirname(__file__)
     with open(os.path.join(c_dir, "config.txt")) as key_file:
@@ -13,7 +13,6 @@ try:
     binance.apiKey = api_key
     binance.secret = secret
     # binance.uid = 'hczj0167'
-    Account = Portfolio()
     available_balance = binance.fetch_free_balance()['USDT']
     btc_holdings = binance.fetch_free_balance()['BTC']
     usdt_value_btc_holdings = btc_holdings * binance.fetch_ticker('BTC/USDT')['close']
@@ -23,24 +22,18 @@ except Exception as e:
     print(f'error instantiating exchange: {e}')
 
 
-def order(symbol, order_type, side, quantity, price):
+def order(ticker, trade_type, direction, amount, price):
     try:
-        print(f'sending order: {symbol} - {order_type} - {side} - {quantity} - {price}')
-        order_receipt = binance.create_order(symbol, order_type, side, quantity, price=None)
+        print(f'sending order: {ticker} - {trade_type} - {direction} - {amount} - {price}')
+        order_receipt = binance.create_order(ticker, trade_type, direction, amount, price)
         return order_receipt
     except Exception as exception:
         print(f"error occurred: {exception.args}")
 
 
-def exit_all_positions():
-    try:
-        order('BTC/USDT', "MARKET", "SELL", btc_holdings, price=None)
-    except TimeoutError:
-        exit_all_positions()
-
-
 # actual web server starts here #
 app = Flask(__name__)
+run_with_ngrok(app)
 
 
 @app.route('/')
@@ -52,7 +45,7 @@ def Dashboard():
                            usdt_balance=available_balance,
                            btc_holdings=binance.fetch_free_balance()['BTC'],
                            total_usdt_value=total_usdt_value,
-                           exit_all_positions=exit_all_positions())
+                           )
 
 # webhook for receiving of orders
 
@@ -72,33 +65,23 @@ def webhook():
 
     # do a check to see if the trade is possible
     if position_size < available_balance:
-        entry_order_response = order(symbol=symbol,
-                                     order_type=order_type,
-                                     side=side,
-                                     quantity=quantity,
+        entry_order_response = order(ticker=symbol,
+                                     trade_type=order_type,
+                                     direction=side,
+                                     amount=quantity,
                                      price=None)
-        if entry_order_response:
-            print(f'Trade executed \n{entry_order_response}')
-            exit_order_response = order(symbol=symbol,
-                                        order_type='LIMIT',
-                                        side=side,
-                                        quantity=quantity,
-                                        price=close*1.025)  # TODO change None to limit price
-            print(f'Take profit submitted: {exit_order_response}')
-        # construct trade details
-        trades = binance.fetch_my_trades('BTC/USDT')
-        last_trade = trades[-1].pop('info')
-        trade = [last_trade['time'],
-                 last_trade['orderId'],
-                 last_trade['symbol'],
-                 last_trade['price'],
-                 side,
-                 order_type,
-                 quantity]
-        df = pd.DataFrame(trade)
+        print(f'entry trade submitted: {entry_order_response}')
 
-        # add executed trade to portfolio
-        Account.ADD_TRADE(df)
+        if entry_order_response:
+            limit_price = round(float(close * 1.025), 2)
+            sell = "SELL"
+            exit_order_response = order(ticker=symbol,
+                                        trade_type='LIMIT',
+                                        direction=sell,
+                                        amount=quantity,
+                                        price=limit_price)  # TODO change None to limit price"""
+            if exit_order_response:
+                print(f'Take profit submitted: {exit_order_response}')
     else:
         webhook_message = None
     return webhook_message
